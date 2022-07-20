4a71f94ec797e1596fea77e11a13ef0114972192
Mihiolus <108616993+Mihiolus@users.noreply.github.com>
2022-07-20 11:13:12 +0300

    Changed sprite loader to use an atlas

--- Assets/Scripts/SpriteLoader.cs
+++ Assets/Scripts/SpriteLoader.cs
@@ -1,21 +1,55 @@
 using UnityEngine;
+using UnityEngine.U2D;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 public interface ISpriteLoader
 {
-    ///<returns> Card sprites sorted from A = 0, to King = 12;
+    ///<returns> Card sprites sorted from A = 1, to King = 13;
     /// suit order not guaranteed </returns>
-    Sprite[] GetSortedSprites();
+    SortedList<int, List<Sprite>> GetSortedSprites();
 }
 
 public class SpriteLoader : MonoBehaviour, ISpriteLoader
 {
     [Header("Sprite source")]
     [SerializeField]
-    [Tooltip("Looks for the texture in the Resources folder")]
-    private Texture2D _cardTexture;
+    [Tooltip("Sprites' names end with a letter or number indicating their rank: A,2-10,J,Q,K")]
+    private SpriteAtlas _atlas;
 
-    public Sprite[] GetSortedSprites()
+    public SortedList<int, List<Sprite>> GetSortedSprites()
     {
-        return Resources.LoadAll<Sprite>(_cardTexture.name);
+        Sprite[] allSprites = new Sprite[_atlas.spriteCount];
+        SortedList<int, List<Sprite>> sortedSprites = new SortedList<int, List<Sprite>>();
+        _atlas.GetSprites(allSprites);
+        foreach (var s in allSprites)
+        {
+            string name = string.Copy(s.name);
+            int index = name.IndexOf("(Clone)");
+            name = name.Remove(index);
+            var match = Regex.Match(name, @"(\d{1,2}|A|J|Q|K)$");
+            if (match.Success)
+            {
+                int value;
+
+                if (!int.TryParse(match.Value, out value))
+                {
+                    switch (match.Value)
+                    {
+                        case "A": value = 1; break;
+                        case "J": value = 11; break;
+                        case "Q": value = 12; break;
+                        case "K": value = 13; break;
+                    }
+                }
+                if (!sortedSprites.ContainsKey(value))
+                {
+                    sortedSprites.Add(value, new List<Sprite>(4));
+                }
+                sortedSprites[value].Add(s);
+            }
+        }
+        return sortedSprites;
     }
 }
